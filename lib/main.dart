3e8c9d14c6ff8209dc70e36034018ff8bb846c35
kaytee124 <ktproductions124@gmail.com>
2025-04-25 02:38:05 +0000

    sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:final_project/Screens/sign_up.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
